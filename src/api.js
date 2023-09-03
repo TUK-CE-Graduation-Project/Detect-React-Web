993ec8fd10ed41cb4dc3465015c0653863394189
jqkk <kkj2551@naver.com>
2023-09-03 20:19:05 +0900

    fix: resolve cors problem

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export async function fetchDataFromAPI() {
     try {
-      const response = await fetch('http://18.207.198.224:8080/api/geotab/search/all'); // API 엔드포인트 URL을 사용자의 API URL로 대체합니다.
+      const response = await fetch('/api/geotab/search/all'); // API 엔드포인트 URL을 사용자의 API URL로 대체합니다.
       if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
